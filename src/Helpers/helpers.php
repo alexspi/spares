1e7df0e7cd07d77b555f7f55f19d40abb189e9a2
alexspi <alexspi1@yandex.ru>
2016-06-30 12:34:20 +0300

    обновил хелпер

--- src/Helpers/helpers.php
+++ src/Helpers/helpers.php
@@ -1,11 +1,37 @@
 <?php
+namespace App\Helpers;
 
-namespace Alexspi\Spares\Helpers;
-
-use Alexspi\Spares\Model\PrsoProduct;
+use Alexspi\Spares\Http\Controllers\PrsoCategoryController;
+use Alexspi\Spares\Http\Controllers\PrsoProductController;
+use Alexspi\Spares\Model\PrsoCategory as PrsoCatecory;
 use Alexspi\Spares\Model\PrsoCategory;
+
 
 class Helper
 {
-   
+    
+    public static function UpdateProductCategori($category_name){
+        $val = explode('\\', $category_name);
+
+        $count = count($val);
+        dd($count);
+        $value = last($val);
+        
+        if (! $category =  PrsoCategory::where('name',$value)->value('id') ){
+
+
+
+            $catnew= new PrsoCategory();
+            $catnew->name=$value;
+            $catnew->parent_id = null;
+            $catnew->save();
+            $category = $catnew->id; 
+        }
+
+
+
+        return $category;
+       // dd($category);
+    }
+    
 }
